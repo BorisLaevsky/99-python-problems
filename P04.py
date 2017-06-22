90c8252c7761d533dbbec8a277c0c200f232c548
BorisLaevsky <borislaevsky@gmail.com>
2017-06-22 18:03:01 +0300

    P04 - how much elements are in the list

--- P04.py
+++ P04.py
@@ -1,5 +1,14 @@
 import SingleLinkedList
 
-def number_of_elements(input_list):
+def number_of_elements(input_list, index = 0):
+    if not input_list:
+        return index
+    elif input_list.next_element():
+        index += 1
+        return number_of_elements(input_list.next_element(), index)
+    else:
+        return index + 1
 
-
+x = SingleLinkedList.Node(1, SingleLinkedList.Node(2, SingleLinkedList.Node(3, None)))
+y = None
+print number_of_elements(y)
