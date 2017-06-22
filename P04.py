310d1e0b90877d6104c4c0d968bfdf9ee836a082
BorisLaevsky <borislaevsky@gmail.com>
2017-06-22 18:18:26 +0300

    deleted unnecessary index+1 line

--- P04.py
+++ P04.py
@@ -4,11 +4,10 @@ def number_of_elements(input_list, index = 0):
     if not input_list:
         return index
     elif input_list.next_element():
-        index += 1
-        return number_of_elements(input_list.next_element(), index)
+        return number_of_elements(input_list.next_element(), index + 1)
     else:
         return index + 1
 
 x = SingleLinkedList.Node(1, SingleLinkedList.Node(2, SingleLinkedList.Node(3, None)))
 y = None
-print number_of_elements(y)
+print number_of_elements(x)
