f352140ad672687556db576a2e568f06af9f80ae
BorisLaevsky <borislaevsky@gmail.com>
2017-07-05 19:46:15 +0300

    P08 eliminate duplicates

---
+++ P08.py
@@ -0,0 +1,19 @@
+from SingleLinkedList import Node
+from P01 import get_last
+
+def compress(input_list):
+    output = Node(None, None)
+    while input_list.next_element():
+        if input_list.value() == input_list.next_element().value():
+            input_list = input_list.next_element()
+        else:
+            get_last(output).set_next(Node(input_list.value(), None))
+            input_list = input_list.next_element()
+    if input_list.value() == get_last(output).value():
+        return output.next_element()
+    else:
+        get_last(output).set_next(input_list)
+        return output.next_element()
+
+x = Node(1,Node(3,Node(3,Node(3,Node(3, None)))))
+print compress(x).next_element()
