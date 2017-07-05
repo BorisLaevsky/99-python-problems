2de10fd5591e8538ecaad0210b79d9e7f7cfe594
BorisLaevsky <borislaevsky@gmail.com>
2017-07-05 19:56:02 +0300

    P08 removed duplicated conditions

--- P08.py
+++ P08.py
@@ -4,16 +4,12 @@ from P01 import get_last
 def compress(input_list):
     output = Node(None, None)
     while input_list.next_element():
-        if input_list.value() == input_list.next_element().value():
-            input_list = input_list.next_element()
-        else:
+        if input_list.value() != input_list.next_element().value():
             get_last(output).set_next(Node(input_list.value(), None))
-            input_list = input_list.next_element()
-    if input_list.value() == get_last(output).value():
-        return output.next_element()
-    else:
+        input_list = input_list.next_element()
+    if input_list.value() != get_last(output).value():
         get_last(output).set_next(input_list)
-        return output.next_element()
+    return output.next_element()
 
 x = Node(1,Node(3,Node(3,Node(3,Node(3, None)))))
-print compress(x).next_element()
+print compress(x).next_element().next_element()
