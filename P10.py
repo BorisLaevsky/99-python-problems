49d145d71decdab17e34de6d07ccdd7ddb029b8d
BorisLaevsky <borislaevsky@gmail.com>
2017-07-06 15:39:20 +0300

    length spelling now correct, output in form of list of tuples

--- P10.py
+++ P10.py
@@ -6,16 +6,16 @@ def lenght_encoding(input_list):
     output = Node(None, None)
     packed = pack(input_list)
     while packed:
-        lenght = 0
+        length = 0
         last = get_last(output)
         sublist = packed.value()
         while sublist:
-            value = packed.value()
-            lenght += 1
+            value = packed.value().value()
+            length += 1
             sublist = sublist.next_element()
-        last.set_next(Node(Node(lenght,Node(value, None)),None))
+        last.set_next(Node((length,value),None))
         packed = packed.next_element()
     return output.next_element()
 
 x = Node(1,Node(1,Node(1,Node(1,Node(1, Node(2,Node(2,Node(2, None))))))))
-print lenght_encoding(x).next_element().value().next_element()
+print lenght_encoding(x)
